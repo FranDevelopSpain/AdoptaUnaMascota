1409c4fd9ea08b363b95ed4ac24b3680e0f8b60e
franciscoappscev <76908849+franciscoappscev@users.noreply.github.com>
2023-03-21 11:03:47 +0100

    Modificaciones finales para la release_1

--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/views/RecoverPasswordActivity.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/views/RecoverPasswordActivity.java
@@ -1,8 +1,6 @@
 package com.tfg.adoptaunamascota.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -20,7 +18,6 @@ public class RecoverPasswordActivity extends AppCompatActivity {
     Button regresarBTN;
 
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
