d2b0fc6731ae2a83c66be3cf6a5a7fd8b262876b
franciscoappscev <76908849+franciscoappscev@users.noreply.github.com>
2023-03-23 21:17:04 +0100

    Cambios en el RegisterService

--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/services/RegisterService.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/services/RegisterService.java
@@ -2,16 +2,13 @@ package com.tfg.adoptaunamascota.services;
 
 import com.tfg.adoptaunamascota.models.Register;
 import com.tfg.adoptaunamascota.models.User;
-
 import retrofit2.Call;
-import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface RegisterService {
 
-    @FormUrlEncoded
     @POST("/register")
     Call<User> USER_CALL_REGISTER(
             Register userData);
@@ -19,14 +16,10 @@ public interface RegisterService {
     @FormUrlEncoded
     @GET("/register/users")
     Call<Register> REGISTER_CALL_USERS(
-            );
-    @FormUrlEncoded
+            Register userData);
     @GET("/register/users/passwords")
     Call<Register> REGISTER_CALL_USERS_PASSWORD(
-            @Field("name") String name,
-            @Field("surname") String surname,
-            @Field("mail") String mail,
-            @Field("password") String password
+            Register userData
     );
 
 }
