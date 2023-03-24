56e5bdd66ff741ce26d000b6b70983f915a37c33
franciscoappscev <76908849+franciscoappscev@users.noreply.github.com>
2023-03-24 20:11:39 +0100

    Cambios menores

--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/repository/UserRepository.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.tfg.adoptaunamascota.repository;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-
 import com.tfg.adoptaunamascota.models.User;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UserRepository {
     private SharedPreferences sharedPreferences;
@@ -11,24 +12,42 @@ public class UserRepository {
     public UserRepository(Context context) {
         sharedPreferences = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE);
     }
+
     public void registerUser(String mail, String password, String name, String surname) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("mail", mail);
-        editor.putString("password", password);
-        editor.putString("name", name);
-        editor.putString("surname", surname);
+
+        // Guardar los datos del usuario en un objeto JSON
+        JSONObject userJson = new JSONObject();
+        try {
+            userJson.put("mail", mail);
+            userJson.put("password", password);
+            userJson.put("name", name);
+            userJson.put("surname", surname);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Almacenar el objeto JSON como String en SharedPreferences usando el correo como clave
+        editor.putString(mail, userJson.toString());
         editor.apply();
     }
+
     public User getUser(String mail, String password) {
-        String userEmail = sharedPreferences.getString("mail", null);
-        String userPassword = sharedPreferences.getString("password", null);
+        String userJsonString = sharedPreferences.getString(mail, null);
+        if (userJsonString != null) {
+            try {
+                JSONObject userJson = new JSONObject(userJsonString);
+                String userEmail = userJson.getString("mail");
+                String userPassword = userJson.getString("password");
 
-        if (userEmail != null && userPassword != null && userEmail.equals(mail) && userPassword.equals(password)) {
-            String userName = sharedPreferences.getString("name", "");
-            String userSurname = sharedPreferences.getString("surname", "");
-            return new User(mail, password, userName, userSurname);
+                if (userEmail.equals(mail) && userPassword.equals(password)) {
+                    String userName = userJson.getString("name");
+                    String userSurname = userJson.getString("surname");
+                    return new User(mail, password, userName, userSurname);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-
         return null;
     }
 }
