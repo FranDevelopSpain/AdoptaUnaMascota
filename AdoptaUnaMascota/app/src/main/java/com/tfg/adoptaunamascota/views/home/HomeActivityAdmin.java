e345fd9a09eb8b4b89b6ec62122d676c3886a9f5
franciscoappscev <76908849+franciscoappscev@users.noreply.github.com>
2023-05-02 18:41:20 +0200

    Se realizan los cambios necesarios para que se vea correctamente las actividades de las home activities y aparece el filtro

--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/views/home/HomeActivityAdmin.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/views/home/HomeActivityAdmin.java
@@ -6,7 +6,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,16 +15,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.tfg.adoptaunamascota.R;
+import com.tfg.adoptaunamascota.adapters.AnimalAdapter;
 import com.tfg.adoptaunamascota.adapters.CustomExpandableListAdapter;
 import com.tfg.adoptaunamascota.adapters.ExpandableListDataPump;
 import com.tfg.adoptaunamascota.models.animals.Animal;
 import com.tfg.adoptaunamascota.models.animals.Cats;
 import com.tfg.adoptaunamascota.models.animals.Dogs;
-import com.tfg.adoptaunamascota.views.home.crudAdmin.UserManagementActivity;
 import com.tfg.adoptaunamascota.views.home.animalview.AnimalDetailActivity;
+import com.tfg.adoptaunamascota.views.home.crudAdmin.UserManagementActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,7 +42,8 @@ public class HomeActivityAdmin extends AppCompatActivity {
     private ExpandableListAdapter expandableListAdapter;
     private List<String> expandableListTitle;
     private HashMap<String, List<String>> expandableListDetail;
-    private LinearLayout animalList;
+    private RecyclerView animalList;
+    private AnimalAdapter animalAdapter;
 
 
     @Override
@@ -51,11 +54,16 @@ public class HomeActivityAdmin extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         animalList = findViewById(R.id.animal_list);
+        animalList.setLayoutManager(new LinearLayoutManager(this));
+        List<Animal> animals = new ArrayList<>();
+        animalAdapter = new AnimalAdapter(animals);
+        animalList.setAdapter(animalAdapter);
         drawerLayout = findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         navigationView = findViewById(R.id.navigation_view);
+        loadInitialAnimals();
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -139,13 +147,8 @@ public class HomeActivityAdmin extends AppCompatActivity {
 
     private void filterAnimalList(String filter) {
         List<Animal> animals = getFilteredAnimals(filter);
-
-        animalList.removeAllViews();
-
-        for (Animal animal : animals) {
-            View animalView = createAdminAnimalView(animal);
-            animalList.addView(animalView);
-        }
+        animalAdapter.setAnimalList(animals);
+        animalAdapter.notifyDataSetChanged();
     }
 
     private List<Animal> getFilteredAnimals(String filter) {
@@ -193,5 +196,20 @@ public class HomeActivityAdmin extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+    private void loadInitialAnimals() {
+        List<Animal> animals = getAllAnimals();
+        animalAdapter.setAnimalList(animals);
+        animalAdapter.notifyDataSetChanged();
+    }
+    private List<Animal> getAllAnimals() {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Dogs("1", "Dog1", "Macho", R.drawable.perro1));
+        animals.add(new Dogs("2", "Dog2", "Macho", R.drawable.perro2));
+        animals.add(new Dogs("3", "Dog", "Macho", R.drawable.perro3));
+        animals.add(new Cats("4", "Cat", "Hembra", R.drawable.gato1));
+        animals.add(new Cats("5", "Cat", "Hembra", R.drawable.gato2));
+        // ... agrega todos los animales aquí
+        return animals;
     }
 }
