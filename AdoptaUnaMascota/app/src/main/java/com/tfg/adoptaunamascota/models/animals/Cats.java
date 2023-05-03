93dffd334f066300609391180a804aa7d14a3ac1
franciscoappscev <76908849+franciscoappscev@users.noreply.github.com>
2023-05-04 00:35:24 +0200

    Correcion de error con la herencia en las clases hijas

--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/models/animals/Cats.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/models/animals/Cats.java
@@ -2,20 +2,33 @@ package com.tfg.adoptaunamascota.models.animals;
 
 public class Cats extends Animal {
 
-    public Cats(Long id, String name, String gender, int edad, int image) {
-        super(id, "Cat", gender, name, edad, "This is a cat description", image, "");
+    public Cats(Long id, String gender, String name, int age, String species, String category, String subcategory, String breed, String image, String description) {
+        super(id, "Cat", gender, name, age, description, image, species, category, subcategory, breed);
+    }
+
+    public Cats(String name, String species, int age) {
+        super(name, species, age);
+        setType("Cat");
+        setDescription("This is a cat description");
+    }
+
+    public Cats(String name, String category, String breed, int age, String description) {
+        super(name, category, breed, age, description);
+        setType("Cat");
     }
 
     public Cats() {
         super();
+        setType("Cat");
+        setDescription("This is a cat description");
+    }
+
+    public String getSize() {
+        return "";
     }
 
     @Override
-    public String getSize() {
-        return ""; // Las clases de gatos no tienen un atributo de tamaño, por lo que simplemente devolvemos una cadena vacía
-    }
-    @Override
     public int getEdadEnMeses() {
-        return getEdad();
+        return getEdad() * 12;
     }
 }
--- AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/models/animals/Dogs.java
+++ AdoptaUnaMascota/app/src/main/java/com/tfg/adoptaunamascota/models/animals/Dogs.java
@@ -1,15 +1,38 @@
 package com.tfg.adoptaunamascota.models.animals;
 
 public class Dogs extends Animal {
+
     private String size;
 
-    public Dogs(Long id, String name, String gender, String size, int age, int image) {
-        super(id, "Dog", gender, name, age, "This is a dog description", image);
+    public Dogs(Long id, String gender, String name, String size, int age, String species, String category, String subcategory, String breed, String image, String description) {
+        super(id, "Dog", gender, name, age, description, image, species, category, subcategory, breed);
+        setType("Dog");
         this.size = size;
     }
 
-    @Override
+    public Dogs(String name, String species, int age) {
+        super(name, species, age);
+        setType("Dog");
+        setDescription("This is a dog description");
+    }
+
+    public Dogs(String name, String category, String breed, int age, String description) {
+        super(name, category, breed, age, description);
+        setType("Dog");
+    }
+
+    public Dogs() {
+        super();
+        setType("Dog");
+        setDescription("This is a dog description");
+    }
+
     public String getSize() {
         return size;
+    }
+
+    @Override
+    public int getEdadEnMeses() {
+        return getEdad() * 12;
     }
 }
